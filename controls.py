b106bdf0f1abad3b646929d97cbce9cbec8de26d
sebastian-wardzinski <52944930+sebastian-wardzinski@users.noreply.github.com>
2020-12-04 13:31:57 -0500

    Handles collisions better, added features
    - Added timeout to move command 
    - Variable start location, based on the current location of the drone
    - '(S)top' and '(C)ontinue' mechanism to toggle the gaze control

--- controls.py
+++ controls.py
@@ -1,10 +1,6 @@
 import airsim
-import keyboard
 import win32gui
-import time
-import sys
-from threading import Thread
-
+import keyboard
 
 # parameters used to detemine the threshold when mapping to cardinal directions
 xlength = 1920
@@ -41,45 +37,67 @@ def calculate_increment(caller, prev_dir, prev_cnt):
 
 
 def main():
+    print("Setup Complete")
     prev_dir = -1
     prev_cnt = 0
-    x_start = y_start = z_start = 0
+
+    # get current drone position
+    position = client.getMultirotorState().kinematics_estimated.position
+    x_start = position.x_val
+    y_start = position.y_val
+    z_start = position.z_val 
 
     while(1):
+        # if key is pressed stop giving inputs until key C is pressed
+        # Note: key S has to be pressed (held down) until the current action has completed (this won't trigger while drone is moving)
+        if keyboard.is_pressed('s'):
+                while not keyboard.is_pressed('c'):
+                    continue
+
         gaze_pos = win32gui.GetCursorPos()
         x = gaze_pos[0]
         y = gaze_pos[1]
 
+        # look right to move right
         if (x > rightbound and (upbound <= y <= downbound)):
             increment, prev_dir, prev_cnt = calculate_increment(0, prev_dir, prev_cnt)
             print("RIGHT ", increment)
             y_start = y_start + increment
-          
+        
+        # look left to move left
         elif (x < leftbound and (upbound <= y <= downbound)):
             increment, prev_dir, prev_cnt = calculate_increment(1, prev_dir, prev_cnt)
             print("LEFT ", increment)
             y_start = y_start - increment
 
+        # look down to move backwards
         elif (y > downbound and (leftbound <= x <= rightbound)):
             increment, prev_dir, prev_cnt = calculate_increment(2, prev_dir, prev_cnt)
             print("BACKWARD ", increment)
             x_start = x_start - increment
             
+        # look up to move forwards
         elif (y < upbound and (leftbound <= x <= rightbound)):
             increment, prev_dir, prev_cnt = calculate_increment(3, prev_dir, prev_cnt)
             print("FORWARD ", increment)
             x_start = x_start + increment
         
+        # look top-right to move up
         elif (y < upbound and x > rightbound):
             increment, prev_dir, prev_cnt = calculate_increment(4, prev_dir, prev_cnt)
             print("UP ", increment)
             z_start = z_start - increment
 
+        # look bottom-left to move down
         elif (y > downbound and x < leftbound):
             increment, prev_dir, prev_cnt = calculate_increment(5, prev_dir, prev_cnt)
             print("DOWN ", increment)
             z_start = z_start + increment
+        
+        # not a valid command, don't try to move drone
+        else:
+            continue
 
-        client.moveToPositionAsync(x_start, y_start, z_start, increment).join()
+        client.moveToPositionAsync(x_start, y_start, z_start, increment, 5).join()
 
 main()

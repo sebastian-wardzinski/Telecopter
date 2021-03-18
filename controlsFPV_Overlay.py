cf6b1e15fdf5e68f6f00eb951bf4802a7eb51000
sebastian-wardzinski <52944930+sebastian-wardzinski@users.noreply.github.com>
2021-03-18 00:39:30 -0400

    UI changes, added speed modes
    - Changed color scheme
    - Changed buttons to rounded rectangles and made the hitboxes match (more or less)
    - speed modes

--- controlsFPV_Overlay.py
+++ controlsFPV_Overlay.py
@@ -1,5 +1,3 @@
-
-
 ################# CONSTANTS #######################
 
 # Can play around with these values to change the size and location of circles for UI, the hitboxes in the logic also use these variables
@@ -7,8 +5,8 @@ RADIUS = 100
 BUTTON_OFFSET = 200
 
 # Can play around with these variables to figure out hold long you have to be within a circle to trigger an action (more-or-less, THRESHOLD * INTERVAL)
-FIXATION_THRESHOLD = 10
-POLLING_INTERVAL = 0.1
+FIXATION_THRESHOLD = 20
+POLLING_INTERVAL = 0.05
 
 ################## GLOBALS ######################
 
@@ -18,8 +16,18 @@ instruction_executing = 0
 # used both in logic to detect gaze and in UI to highlight the circle on which the gaze currently lands (if any)
 curr_circle = 0
 
-################# OVERLAY #######################
+x = 0
+y = 0
 
+from enum import Enum
+class Mode(Enum):
+    LONG_DISTANCE = 0
+    NORMAL = 1
+    SHORT_DISTANCE = 2
+
+mode = Mode.LONG_DISTANCE
+
+################# OVERLAY #######################
 import sys
 import threading
 
@@ -38,47 +46,65 @@ class CustomWindow(QMainWindow):
         painter = QPainter(self)
         
         if showGuides:
+            painter.setPen(QPen(Qt.black, 0, Qt.SolidLine))
+            centerPoint = QDesktopWidget().availableGeometry(0).center()
+            
+            # To draw over the cursor, sucks for a few reasons
+            """
+            - doesn't move when the drone is performing action
+            - can't click on things (you end up clicking on the circle rather than whats behind it)
+            - the mouse cursor is still visible
+            painter.setOpacity(0.8)
+            painter.setBrush(QBrush(Qt.gray, Qt.SolidPattern))
+            painter.drawEllipse(QPoint(x,y), RADIUS / 3, RADIUS / 3)
+            """
+
             painter.setOpacity(0.4)
-            painter.setPen(QPen(Qt.black, 5, Qt.SolidLine))
+            painter.setBrush(QBrush(Qt.green, Qt.SolidPattern))
+            # if the current gaze lands on a button, highlight that button
+            if curr_circle == Button.Center:
+                painter.drawRoundedRect(centerPoint.x() - RADIUS, centerPoint.y() - RADIUS, RADIUS * 2, RADIUS * 2, 15, 15)
+            elif curr_circle == Button.Left:
+                painter.drawRoundedRect(centerPoint.x() - RADIUS - BUTTON_OFFSET, centerPoint.y() - RADIUS, RADIUS * 2, RADIUS * 2, 15, 15)
+            elif curr_circle == Button.Right:
+                painter.drawRoundedRect(centerPoint.x() - RADIUS + BUTTON_OFFSET, centerPoint.y() - RADIUS, RADIUS * 2, RADIUS * 2, 15, 15)
+            elif curr_circle == Button.Up:
+                painter.drawRoundedRect(centerPoint.x() - RADIUS, centerPoint.y() - RADIUS - BUTTON_OFFSET, RADIUS * 2, RADIUS * 2, 15, 15)
+            elif curr_circle == Button.Down: 
+                painter.drawRoundedRect(centerPoint.x() - RADIUS, centerPoint.y() - RADIUS + BUTTON_OFFSET, RADIUS * 2, RADIUS * 2, 15, 15)
 
             # buttons are different colors depending on if an instruction is executing or not
             if instruction_executing:
                 painter.setBrush(QBrush(Qt.gray, Qt.SolidPattern))
             else:
-                painter.setBrush(QBrush(Qt.red, Qt.SolidPattern))
-            
-            centerPoint = QDesktopWidget().availableGeometry(0).center()
-            painter.drawEllipse(centerPoint, RADIUS, RADIUS)
-            painter.drawEllipse(centerPoint - QPoint(BUTTON_OFFSET, 0), RADIUS, RADIUS)
-            painter.drawEllipse(centerPoint + QPoint(BUTTON_OFFSET, 0), RADIUS, RADIUS)
-            painter.drawEllipse(centerPoint - QPoint(0, BUTTON_OFFSET), RADIUS, RADIUS)
-            painter.drawEllipse(centerPoint + QPoint(0, BUTTON_OFFSET), RADIUS, RADIUS)
+                painter.setBrush(QBrush(Qt.blue, Qt.SolidPattern))
 
+            if curr_circle != Button.Center: 
+                painter.drawRoundedRect(centerPoint.x() - RADIUS, centerPoint.y() - RADIUS, RADIUS * 2, RADIUS * 2, 15, 15)
+            if curr_circle != Button.Left:
+                painter.drawRoundedRect(centerPoint.x() - RADIUS - BUTTON_OFFSET, centerPoint.y() - RADIUS, RADIUS * 2, RADIUS * 2, 15, 15)
+            if curr_circle != Button.Right:
+                painter.drawRoundedRect(centerPoint.x() - RADIUS + BUTTON_OFFSET, centerPoint.y() - RADIUS, RADIUS * 2, RADIUS * 2, 15, 15)
+            if curr_circle != Button.Up:
+                painter.drawRoundedRect(centerPoint.x() - RADIUS, centerPoint.y() - RADIUS - BUTTON_OFFSET, RADIUS * 2, RADIUS * 2, 15, 15)
+            if curr_circle != Button.Down:
+                painter.drawRoundedRect(centerPoint.x() - RADIUS, centerPoint.y() - RADIUS + BUTTON_OFFSET, RADIUS * 2, RADIUS * 2, 15, 15)
 
-            # if the current gaze lands on a button, highlight that button
-            painter.setBrush(QBrush(Qt.green, Qt.SolidPattern))
-
-            if curr_circle == Button.Center:
-                painter.drawEllipse(centerPoint, RADIUS, RADIUS)
-            elif curr_circle == Button.Left:
-                painter.drawEllipse(centerPoint - QPoint(BUTTON_OFFSET, 0), RADIUS, RADIUS)
-            elif curr_circle == Button.Right:
-                painter.drawEllipse(centerPoint + QPoint(BUTTON_OFFSET, 0), RADIUS, RADIUS)
-            elif curr_circle == Button.Up:
-                painter.drawEllipse(centerPoint - QPoint(0, BUTTON_OFFSET), RADIUS, RADIUS)
-            elif curr_circle == Button.Down: 
-                painter.drawEllipse(centerPoint + QPoint(0, BUTTON_OFFSET), RADIUS, RADIUS)
-
-            # write text in the middle of each button (TODO: make text larger)
+            # write text in the middle of each button
             if showDescriptions:
                 centerPoint = QDesktopWidget().availableGeometry(0).center()
 
-                painter.setOpacity(1)
-                painter.drawText(centerPoint.x() - 20, centerPoint.y(), "Forward")
-                painter.drawText(centerPoint.x() - BUTTON_OFFSET - 15, centerPoint.y(), "R.Left")
-                painter.drawText(centerPoint.x() + BUTTON_OFFSET - 15, centerPoint.y(), "R.Right")
-                painter.drawText(centerPoint.x() - 5, centerPoint.y() - BUTTON_OFFSET, "Up")
-                painter.drawText(centerPoint.x() - 10, centerPoint.y() + BUTTON_OFFSET, "Down")
+                font = painter.font()
+                font.setPointSize(font.pointSize() * 4)
+                painter.setFont(font)
+                
+                centerPoint.setY(centerPoint.y() + 12)
+
+                painter.drawText(centerPoint.x() - 75, centerPoint.y(), "Forward")
+                painter.drawText(centerPoint.x() - BUTTON_OFFSET - 60, centerPoint.y(), "R.Left")
+                painter.drawText(centerPoint.x() + BUTTON_OFFSET - 65, centerPoint.y(), "R.Right")
+                painter.drawText(centerPoint.x() - 25, centerPoint.y() - BUTTON_OFFSET, "Up")
+                painter.drawText(centerPoint.x() - 55, centerPoint.y() + BUTTON_OFFSET, "Down")
 
 
 def toggleButtonClicked():
@@ -160,10 +186,6 @@ client.armDisarm(True)
 
 client.takeoffAsync().join()
 
-base_speed = 8
-rotate = 22.5
-
-
 def calculate_increment(caller, prev_dir, prev_cnt):
     # update momentum variables
     if caller != prev_dir:
@@ -177,14 +199,30 @@ def calculate_increment(caller, prev_dir, prev_cnt):
     instruction_executing = 1
     window.update()
 
+    base_speed = rotate = 0
+    if mode == Mode.NORMAL:
+        base_speed = 8
+        rotate = 22.5
+    elif mode == Mode.SHORT_DISTANCE:
+        base_speed = 4
+        rotate = 30
+    elif mode == Mode.LONG_DISTANCE:
+        base_speed = 16
+        rotate = 18
+
+    returnValue = 0
+    if caller == 1 or caller == 2:
+        returnValue = rotate
+    else:
+        returnValue = base_speed * 1.5 ** prev_cnt
     # calculate position increment based on momemtum
-    return base_speed * 1.5 ** prev_cnt, prev_dir, prev_cnt
+    return returnValue, prev_dir, prev_cnt
 
 
-def renormalize(x_start, y_start):
+def renormalize(x_start, y_start, speed):
     position = client.getMultirotorState().kinematics_estimated.position
     z_start = position.z_val
-    client.moveToPositionAsync(x_start, y_start, z_start, base_speed, 5).join()
+    client.moveToPositionAsync(x_start, y_start, z_start, speed, 5).join()
 
 
 def print_command(prev_dir, increment):
@@ -230,24 +268,27 @@ def main():
         x_start = position.x_val
         y_start = position.y_val 
         z_start = position.z_val
-        increment = rotate
+        increment = 0
 
         gaze_pos = win32gui.GetCursorPos()
+
+        global x
+        global y
         x = gaze_pos[0]
         y = gaze_pos[1]
 
         global curr_circle
-    
+
         # Find position of current point
-        if math.sqrt((y - height / 2) ** 2 + (x - (width / 2 - BUTTON_OFFSET)) ** 2) < RADIUS:
+        if width / 2 - 3 * RADIUS < x < width / 2 - RADIUS and height / 2 - RADIUS < y < height / 2 + RADIUS:
             curr_circle = Button.Left
-        elif math.sqrt((y - height / 2) ** 2 + (x - (width / 2 + BUTTON_OFFSET)) ** 2) < RADIUS:
+        elif width / 2 + RADIUS < x < width / 2 + 3 * RADIUS and height / 2 - RADIUS < y < height / 2 + RADIUS:
             curr_circle = Button.Right 
-        elif math.sqrt((y - (height / 2 + BUTTON_OFFSET)) ** 2 + (x - width / 2) ** 2) < RADIUS:
+        elif width / 2 - RADIUS < x < width / 2 + RADIUS and height / 2 + RADIUS < y < height / 2 + 3 * RADIUS:
             curr_circle = Button.Down
-        elif math.sqrt((y - (height / 2 - BUTTON_OFFSET)) ** 2 + (x - width / 2) ** 2) < RADIUS:
+        elif width / 2 - RADIUS < x < width / 2 + RADIUS and height / 2 - 3 * RADIUS < y < height / 2 - RADIUS:
             curr_circle = Button.Up
-        elif math.sqrt((y - height / 2) ** 2 + (x - width / 2) ** 2) < RADIUS:
+        elif width / 2 - RADIUS < x < width / 2 + RADIUS and height / 2 - RADIUS < y < height / 2 + RADIUS:
             curr_circle = Button.Center
         else:
             curr_circle = Button.Other
@@ -265,14 +306,14 @@ def main():
         if same_circle_count >= FIXATION_THRESHOLD:
             # LEFT
             if prev_circle == Button.Left and curr_circle == Button.Left:
-                _, prev_dir, prev_cnt = calculate_increment(1, prev_dir, prev_cnt)
+                increment, prev_dir, prev_cnt = calculate_increment(1, prev_dir, prev_cnt)
                 client.rotateByYawRateAsync(-increment, 1).join()
-                renormalize(x_start, y_start) # normalize x and y position after doing a rotation, otherwise it drifts
+                renormalize(x_start, y_start, increment) # normalize x and y position after doing a rotation, otherwise it drifts
             # RIGHT
             elif prev_circle == Button.Right and  curr_circle == Button.Right:
-                _, prev_dir, prev_cnt = calculate_increment(2, prev_dir, prev_cnt)
+                increment, prev_dir, prev_cnt = calculate_increment(2, prev_dir, prev_cnt)
                 client.rotateByYawRateAsync(increment, 1).join()
-                renormalize(x_start, y_start)
+                renormalize(x_start, y_start, increment)
             # DOWN
             elif prev_circle == Button.Down and curr_circle == Button.Down:
                 increment, prev_dir, prev_cnt = calculate_increment(3, prev_dir, prev_cnt)
@@ -310,7 +351,9 @@ def main():
         # If instruction happened, reset buffer and print instruction
         prev_circle = Button.Other
         same_circle_count = 0
-        print_command(prev_dir, increment)
+
+        if increment > 0:
+            print_command(prev_dir, increment)
 
 
 # Run the application
